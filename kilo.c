8ff190875149f2524cf23a433a57a5ed339d2b55
Thomas Rettig <thomas.rettig.11@gmail.com>
2021-12-31 16:43:19 +0800

    Update kilo.c

--- kilo.c
+++ kilo.c
@@ -105,6 +105,7 @@ void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
     abAppend(ab, "~", 1);
+    abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
       abAppend(ab, "\r\n", 2);
     }
