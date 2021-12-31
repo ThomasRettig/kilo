ca08c0a257f0f99159ec974d25645ce14b8d6b6d
Thomas Rettig <thomas.rettig.11@gmail.com>
2021-12-31 17:17:45 +0800

    Update kilo.c

--- kilo.c
+++ kilo.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -137,6 +138,17 @@ int getWindowSize(int *rows, int *cols) {
     *rows = ws.ws_row;
     return 0;
   }
+}
+
+/*** file i/o ***/
+void editorOpen() {
+  char *line = "Hello, world!";
+  ssize_t linelen = 13;
+  E.row.size = linelen;
+  E.row.chars = malloc(linelen + 1);
+  memcpy(E.row.chars, line, linelen);
+  E.row.chars[linelen] = '\0';
+  E.numrows = 1;
 }
 
 /*** append buffer ***/
